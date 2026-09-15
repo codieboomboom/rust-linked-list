830ca0a21c6b02d59f31c69949d54f45be5a645a
Anh Tu Do <anhtu.do1998@gmail.com>
2026-09-15 22:07:04 +0800

    Implemented Iter not mut

--- src/second.rs
+++ src/second.rs
@@ -23,6 +23,9 @@ struct Node<T> {
 }
 
 pub struct IntoIter<T>(List<T>); // wrap around List and consume it
+pub struct Iter<'a, T> {
+    next: Option<&'a Node<T>>,
+}
 
 impl<T> List<T> {
     pub fn new() -> Self {
@@ -64,6 +67,27 @@ impl<T> List<T> {
     pub fn into_iter(self) -> IntoIter<T> {
         IntoIter(self)
     }
+
+    pub fn iter<'a>(&'a self) -> Iter<'a, T> {
+        Iter {
+            next: self.head.as_deref().map(|node| &*node)
+        }
+    }
+
+// if apply liftime elision
+/*
+impl<T> List<T> {
+    pub fn iter(&self) -> Iter<T> {
+        Iter { next: self.head.as_deref() }
+    }
+}
+impl<T> List<T> {
+    pub fn iter(&self) -> Iter<T> {
+        Iter { next: self.head.as_deref() }
+    }
+}
+
+ */
 }
 
 impl<T> Iterator for IntoIter<T> {
@@ -71,6 +95,17 @@ impl<T> Iterator for IntoIter<T> {
     fn next(&mut self) -> Option<Self::Item> {
         //access fields of a tuple struct numerically
         self.0.pop() // pop the list (list is field 0)
+    }
+}
+
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.map(|node| {
+            self.next = node.next.as_deref().map(|node| &*node);
+            &node.elem
+        })
     }
 }
 
@@ -136,6 +171,17 @@ mod test {
         assert_eq!(iter.next(), Some(2));
         assert_eq!(iter.next(), Some(1));
         assert_eq!(iter.next(), None);
+    }
+
+    #[test]
+    fn iter() {
+        let mut list = List::new();
+        list.push(1); list.push(2); list.push(3);
+
+        let mut iter = list.iter();
+        assert_eq!(iter.next(), Some(&3));
+        assert_eq!(iter.next(), Some(&2));
+        assert_eq!(iter.next(), Some(&1));
     }
 
 }
